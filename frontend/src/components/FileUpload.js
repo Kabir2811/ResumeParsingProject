6c87dbee0af5341dcf7de6f457b33bd4b885c8d6
Kabir Pandey <109139577+Kabir2811@users.noreply.github.com>
2023-09-08 12:41:34 +0530

    V.1.2

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -44,15 +44,17 @@
 import React, { useState } from 'react';
 import axios from '../axiosConfig';
 import { useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
 
 function FileUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
   const navigate = useNavigate();
-  
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const onDrop = (acceptedFiles) => {
+    setSelectedFile(acceptedFiles[0]);
   };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const handleUpload = () => {
     if (!selectedFile) {
@@ -66,26 +68,61 @@ function FileUpload() {
       .post('http://localhost:8080/student/parse-file', formData)
       .then((response) => {
         console.log('File uploaded:', response.data);
-        const idn = response.data; // Get the last inserted student ID
-        
+        const idn = response.data;
 
-        // Use the 'lastInsertedId' for some purpose, e.g., displaying it
-        
-
-        // Assuming the server responds with the 'id' of the uploaded file
-        navigate(`/student-form/${idn}`); // Use the 'idn' variable here
+        navigate(`/student-form/${idn}`);
       })
       .catch((error) => {
         console.error('Error uploading file:', error);
-        // Optionally, show an error message to the user
       });
   };
 
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  };
+
+  const dropzoneStyle = {
+    width: '300px',
+    height: '200px',
+    borderWidth: '2px',
+    borderColor: isDragActive ? 'green' : 'gray',
+    borderStyle: 'dashed',
+    borderRadius: '5px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+  };
+
+  const buttonStyle = {
+    marginTop: '20px',
+    padding: '10px 20px',
+    background: '#007BFF',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+    borderRadius: '5px',
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <h2>Upload Resume</h2>
-      <input type="file" onChange={handleFileChange} accept=".pdf,.doc,.docx" />
-      <button onClick={handleUpload} disabled={!selectedFile}>
+      <div {...getRootProps()} style={dropzoneStyle}>
+        <input {...getInputProps()} accept=".pdf,.doc,.docx" />
+        {isDragActive ? (
+          <p>Drop the file here...</p>
+        ) : (
+          <p>Drag and drop a resume file here, or click to select one</p>
+        )}
+      </div>
+      {selectedFile && (
+        <p>Selected File: {selectedFile.name}</p>
+      )}
+      <button onClick={handleUpload} disabled={!selectedFile} style={buttonStyle}>
         Upload
       </button>
     </div>
@@ -93,3 +130,5 @@ function FileUpload() {
 }
 
 export default FileUpload;
+
+
